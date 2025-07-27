4bd7a98c0ecae5a44c94bdbc7f351c0a823f97d7
jowal-islam <ranabrindabon@gmail.com>
2025-07-27 23:07:10 +0600

    2nd commit

--- module14/ifelse.js
+++ module14/ifelse.js
@@ -1,8 +1,23 @@
 
-var tricetPrice = 30;
+// var tricetPrice = 30;
 
-if (tricetPrice < 25){
-   console.log("ami mela te jabo")
-} else {
-    console.log("ami mela te jabo na")
-}cl
+// if (tricetPrice < 25){
+//    console.log("ami mela te jabo")
+// } else {
+//     console.log("ami mela te jabo na")
+// }
+
+// let age = 25;
+// if (age > 18 && age < 30) {
+//   console.log("You are eligible.");
+// }
+
+// let role = "admin";
+// if(role === "admin" || role === "moderator"){
+//   console.log('you have access')
+// }
+
+let isLoggedIn = false;
+if(!isLoggedIn){
+  console.log('plaase log in');
+}
