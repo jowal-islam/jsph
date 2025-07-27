d97fd22fad48c76c2d55d5c33b27cf4cdae56496
jowal-islam <ranabrindabon@gmail.com>
2025-07-27 20:45:18 +0600

    all js practise file

---
+++ module14/comparijon.js
@@ -0,0 +1,11 @@
+
+// var price = 23;
+// var komolaPrice = 22;
+// console.log(price == komolaPrice);
+// console.log(5 < 4);
+// console.log(5==6);
+var price = 21;
+var normalPrice = "21";
+// console.log(price==normalPrice);
+// console.log(price===normalPrice);
+console.log(price!=normalPrice);
---
+++ module14/ifelse.js
@@ -0,0 +1,8 @@
+
+var tricetPrice = 30;
+
+if (tricetPrice < 25){
+   console.log("ami mela te jabo")
+} else {
+    console.log("ami mela te jabo na")
+}cl
