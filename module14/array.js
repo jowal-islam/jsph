6e61e482625041c1848bd281f424df0f2c6039ce
jowal-islam <ranabrindabon@gmail.com>
2025-07-30 23:10:20 +0600

    change the array value

--- module14/array.js
+++ module14/array.js
@@ -1,7 +1,13 @@
 
 // const names = ['ratul', 'saltana', 'mishkatul', ]
 // console.log(names.length);
- let mixed = [42, "hello", true, null];
-for (let i = 0; i < mixed.length; i++){
-    console.log(mixed[i]);
-}
+//  let mixed = [42, "hello", true, null];
+// for (let i = 0; i < mixed.length; i++){
+//     console.log(mixed[i]);
+// }
+// ......,,,,,,,,,,,,,,,,,,,,,,,,,,,
+let fruits = ['apple', "banana", 'mango'];
+console.log(fruits[0]);
+console.log(fruits[1]);
+console.log(fruits[2]);
+fruits[1] = 'orange';
