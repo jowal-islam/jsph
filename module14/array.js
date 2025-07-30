5d1a198ad943f27ed5a64fcf1c23c48c5d1e5c1e
jowal-islam <ranabrindabon@gmail.com>
2025-07-30 22:56:20 +0600

    js arrar

---
+++ module14/array.js
@@ -0,0 +1,7 @@
+
+// const names = ['ratul', 'saltana', 'mishkatul', ]
+// console.log(names.length);
+ let mixed = [42, "hello", true, null];
+for (let i = 0; i < mixed.length; i++){
+    console.log(mixed[i]);
+}
