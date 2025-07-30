9ea75c815b28cd881cac305ff38abd0e03d59508
jowal-islam <ranabrindabon@gmail.com>
2025-07-30 23:25:22 +0600

    pop and push

--- module14/array.js
+++ module14/array.js
@@ -6,8 +6,25 @@
 //     console.log(mixed[i]);
 // }
 // ......,,,,,,,,,,,,,,,,,,,,,,,,,,,
-let fruits = ['apple', "banana", 'mango'];
-console.log(fruits[0]);
-console.log(fruits[1]);
-console.log(fruits[2]);
-fruits[1] = 'orange';
+// let fruits = ['apple', "banana", 'mango',"challange"];
+// // console.log(fruits[0]);
+// console.log(fruits[1]);
+// console.log(fruits[2]);
+// fruits[0] = 'orange';
+// fruits[3] = 'poriborton';
+// console.log(fruits.length);
+// console.log(fruits);
+
+
+// let fruits = ['apple', 'banana'];
+// fruits.push('mango');
+// fruits.pop('apple');
+// console.log(fruits);
+
+let books = ['English', 'Math'];
+books.push('science');
+books.push('history');
+console.log(books);
+let removeBook = books.pop();
+console.log(removeBook);
+console.log(books);
