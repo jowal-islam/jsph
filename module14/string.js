a0bbed27e3753ca639afe0b930ebf60de969954e
jowal-islam <ranabrindabon@gmail.com>
2025-07-31 22:08:32 +0600

    some task

--- module14/string.js
+++ module14/string.js
@@ -9,9 +9,15 @@
 //     console.log('not same');
 // }
 
-const address = 'andorkilla';
-// console.log(address.slice(2,5));
+// const address = 'andorkilla';
+// // console.log(address.slice(2,5));
 
-let text = 'mango, banana, licho';
-const array = [...text]
-console.log(array);
+// let text = 'mango, banana, licho';
+// const array = [...text]
+// console.log(array);
+
+
+let sentence = 'ami ajke onk moja korlam';
+let word = sentence.split(" ");
+let lenghtMaser = word.length;
+console.log(lenghtMaser);
