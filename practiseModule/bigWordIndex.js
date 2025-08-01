66aafb6ca41fed471f1f529504d14b818042a415
jowal-islam <ranabrindabon@gmail.com>
2025-08-01 22:44:46 +0600

    big word with index

---
+++ practiseModule/bigWordIndex.js
@@ -0,0 +1,13 @@
+
+let sentence = "ami bangladesher manush ke onek bhalobashi";
+let words = sentence.split(" ");
+let longest = "";
+let index = -1;
+for(let i = 0; i < words.length; i++){
+    if(words[i].length > longest.length){
+        longest = words[i];
+        index = i;
+    }
+}
+let result = longest
+console.log(result, index);
