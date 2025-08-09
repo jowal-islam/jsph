7d4e4ae6ed4eab5e136695fb0ca3fbb35da4dcd3
jowal-islam <ranabrindabon@gmail.com>
2025-08-09 11:14:07 +0600

    problem 01

--- jsNodeFile/jsall.js
+++ jsNodeFile/jsall.js
@@ -1,10 +1,39 @@
 
-// js practise of array
+ // js practise of array
 
 
 
-const arr1 = ["Cecilie", "Lone"];
-const arr2 = ["Emil", "Tobias", "Linus"];
+// const arr1 = ["Cecilie", "Lone"];
+// const arr2 = ["Emil", "Tobias", "Linus"];
 
-const children = arr2.concat(arr1);
-console.log(children)
+// const children = arr2.concat(arr1);
+// console.log(children)
+
+// const arr3 = ["Cecilie", "Lone"];
+// const arr4 = ["Emil", "Tobias", "Linus"];
+// const arr5 = ["Robin"];
+
+// const children1 = arr3.concat(arr4, arr5); 
+// console.log(children1);
+
+// const arr6 = ["Cecilie", "Lone"];
+// const arr7 = [1, 2, 3];
+
+// const arr8 = arr6.concat(arr7); 
+// console.log(arr8);
+
+// problem solve 01
+let nums1 = [1,2,3];
+let nums2 = [2,3,4,5];
+let nums3 = [5,6,7];
+let assmble = nums1.concat(nums2, nums3);
+
+// let newArray = [];
+// for(let i = 0; i < assmble.length; i++){
+//     if(!newArray.includes(assmble[i])){
+//         newArray.push(assmble[i]);
+//     }
+// }
+// console.log(newArray.sort((a, b)=> a - b));
+
+
